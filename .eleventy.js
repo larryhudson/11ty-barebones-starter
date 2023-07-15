d7e3bf4cae8e7c5ad08dac6f03c25b0b2e4fee9f
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2023-07-15 12:07:56 +1000

    changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,30 @@
+// const { generatePdf } = require("./utils/generate-pdf");
+const { generateOgImages } = require("./utils/generate-og-images");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+
+  eleventyConfig.on("eleventy.after", ({ results }) => {
+    // generatePdf({
+    //   pdfsToGenerate: [
+    //     { inputUrl: "/pdf-input/", outputUrl: "/pdf.pdf" },
+    //     {
+    //       inputUrl: "/pdf-input-but-different/",
+    //       outputUrl: "/pdf-different.pdf",
+    //     },
+    //   ],
+    //   outputDir: "./11ty-output",
+    //   serverPort: 8090,
+    // });
+
+    console.log(results);
+
+    generateOgImages({
+      srcJsonPath: "/og-images.json",
+      outputDir: "./11ty-output",
+      serverPort: 8090,
+    });
+  });
 
   return {
     dir: {
